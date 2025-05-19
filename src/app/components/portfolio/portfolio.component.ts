6e7bcb5dc0c3afe01c58e73706e5b370ea686091
Shiwank <shiwank@MacBookPro.lan>
2025-05-19 13:58:10 +0530

    updated portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ProjectInfoComponent } from "../project-info/project-info.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [ProjectInfoComponent],
+  imports: [ProjectInfoComponent,RouterLink],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
